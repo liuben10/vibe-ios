b72534e091aa9825981fe26def7620d0683bafe5
liuben10 <liuben10@gmail.com>
2017-11-20 21:27:38 -0800

    Add Bluetooth, and gitignore autogenerated things

--- Vibe_Prototype2/ViewController.swift
+++ Vibe_Prototype2/ViewController.swift
@@ -7,22 +7,194 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate{
 
     @IBAction func doesSomething(_ sender: Any) {
         print("Wow, this button does something!")
     }
+    let temperatureLabelFontName = "HelveticaNeue-Thin"
+    let temperatureLabelFontSizeMessage:CGFloat = 56.0
+    let temperatureLabelFontSizeTemp:CGFloat = 81.0
+    let sensorTagName = "SensorTag"
+
+    let timerPauseInterval:TimeInterval = 10.0
+    let timerScanInterval:TimeInterval = 2.0
+    var keepScanning = false
+    var state = "OK"
+    var centralManager:CBCentralManager!
+    var sensorTag:CBPeripheral?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
         
-        //var users = Const.getNearbyUsers()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func pauseScan() {
+        // Scanning uses up battery on phone, so pause the scan process for the designated interval.
+        print("*** PAUSING SCAN...")
+        _ = Timer(timeInterval: timerPauseInterval, target: self, selector: #selector(resumeScan), userInfo: nil, repeats: false)
+        centralManager.stopScan()
+//        disconnectButton.isEnabled = true
+    }
+    
+    @objc func resumeScan() {
+        if keepScanning {
+            // Start scanning again...
+            print("*** RESUMING SCAN!")
+//            disconnectButton.isEnabled = false
+//            temperatureLabel.font = UIFont(name: temperatureLabelFontName, size: temperatureLabelFontSizeMessage)
+//            temperatureLabel.text = "Searching"
+//            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+//            disconnectButton.isEnabled = true
+        }
+    }
+    
+    private func centralManager(_ central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], rssi: NSNumber) {
+        print("centralManager didDiscoverPeripheral - CBAdvertisementDataLocalNameKey is \"\(CBAdvertisementDataLocalNameKey)\"")
+        
+        // Retrieve the peripheral name from the advertisement data using the "kCBAdvDataLocalName" key
+        if let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            print("NEXT PERIPHERAL NAME: \(peripheralName)")
+            print("NEXT PERIPHERAL UUID: \(peripheral.identifier.uuidString)")
+            
+            if peripheralName.lowercased().range(of: sensorTagName) != nil {
+                print("SENSOR TAG FOUND! ADDING NOW!!!")
+                // to save power, stop scanning for other devices
+//                keepScanning = false
+//                disconnectButton.isEnabled = true
+                
+                // save a reference to the sensor tag
+                sensorTag = peripheral
+                sensorTag!.delegate = self
+                
+                // Request a connection to the peripheral
+                centralManager.connect(sensorTag!, options: nil)
+            }
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("**** SUCCESSFULLY CONNECTED TO SENSOR TAG!!!")
+        
+//        temperatureLabel.font = UIFont(name: temperatureLabelFontName, size: temperatureLabelFontSizeMessage)
+//        temperatureLabel.text = "Connected"
+        
+        // Now that we've successfully connected to the SensorTag, let's discover the services.
+        // - NOTE:  we pass nil here to request ALL services be discovered.
+        //          If there was a subset of services we were interested in, we could pass the UUIDs here.
+        //          Doing so saves battery life and saves time.
+        peripheral.discoverServices(nil)
+    }
+    
+    /*
+     Invoked when the central manager fails to create a connection with a peripheral.
+     This method is invoked when a connection initiated via the connectPeripheral:options: method fails to complete.
+     Because connection attempts do not time out, a failed connection usually indicates a transient issue,
+     in which case you may attempt to connect to the peripheral again.
+     */
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        print("**** CONNECTION TO SENSOR TAG FAILED!!!")
+    }
+    
+    
+    /*
+     Invoked when an existing connection with a peripheral is torn down.
+     
+     This method is invoked when a peripheral connected via the connectPeripheral:options: method is disconnected.
+     If the disconnection was not initiated by cancelPeripheralConnection:, the cause is detailed in error.
+     After this method is called, no more methods are invoked on the peripheral device’s CBPeripheralDelegate object.
+     
+     Note that when a peripheral is disconnected, all of its services, characteristics, and characteristic descriptors are invalidated.
+     */
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        print("**** DISCONNECTED FROM SENSOR TAG!!!")
+        //        lastTemperature = 0
+        //        updateBackgroundImageForTemperature(lastTemperature)
+        //        circleView.hidden = true
+        //        temperatureLabel.font = UIFont(name: temperatureLabelFontName, size: temperatureLabelFontSizeMessage)
+        //        temperatureLabel.text = "Tap to search"
+        //        humidityLabel.text = ""
+        //        humidityLabel.hidden = true
+        if error != nil {
+            print("****** DISCONNECTION DETAILS: \(error!.localizedDescription)")
+        }
+        sensorTag = nil
+    }
+    
+    //MARK: - CBPeripheralDelegate methods
+    
+    /*
+     Invoked when you discover the peripheral’s available services.
+     
+     This method is invoked when your app calls the discoverServices: method.
+     If the services of the peripheral are successfully discovered, you can access them
+     through the peripheral’s services property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    // When the specified services are discovered, the peripheral calls the peripheral:didDiscoverServices: method of its delegate object.
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        if error != nil {
+            print("ERROR DISCOVERING SERVICES: \(error?.localizedDescription)")
+            return
+        }
+        
+        // Core Bluetooth creates an array of CBService objects —- one for each service that is discovered on the peripheral.
+        if let services = peripheral.services {
+            for service in services {
+                print("Discovered service \(service)")
+                // If we found either the temperature or the humidity service, discover the characteristics for those services.
+//                if (service.uuid == CBUUID(string: Device.TemperatureServiceUUID)) ||
+//                    (service.uuid == CBUUID(string: Device.HumidityServiceUUID)) {
+//                    peripheral.discoverCharacteristics(nil, for: service)
+//                }
+            }
+        }
+    }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        var showAlert = true
+        var message = ""
+        
+        switch central.state {
+        case .poweredOn:
+            // 1
+            keepScanning = true
+            // 2
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            // 3
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        case .poweredOff:
+            state = "Bluetooth on this device is currently powered off."
+        case .unsupported:
+            state = "This device does not support Bluetooth Low Energy."
+        case .unauthorized:
+            state = "This app is not authorized to use Bluetooth Low Energy."
+        case .resetting:
+            state = "The BLE Manager is resetting; a state update is pending."
+        case .unknown:
+            state = "The state of the BLE Manager is unknown."
+        }
+        
+        if showAlert {
+            let alertController = UIAlertController(title: "Central Manager State", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            self.show(alertController, sender: self)
+        }
     }
 
 
